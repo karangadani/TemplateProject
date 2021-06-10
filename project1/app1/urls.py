64e9054999b58535f9ef1580a9f9798b1437630a
Karan <karangadani@gmail.com>
2021-06-10 10:34:10 +0530

    Auth Complete

--- project1/app1/urls.py
+++ project1/app1/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import *
+
+app_name = 'Home'
+
 urlpatterns = [
-        path('',index),
-        path('common/',common),
+        path('',index, name='Home'),
+        
 ]
