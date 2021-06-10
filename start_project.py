2e1c5bcf128c196323b6e31317974badc767896b
Karan <karangadani@gmail.com>
2021-06-10 11:41:28 +0530

    'start'

--- start_project.py
+++
@@ -1,6 +0,0 @@
-import os
-
-WORKDIR = os.getcwd()
-
-os.chdir(WORKDIR)
-os.system(f"pip install virtualenv && virtualenv venv && venv\\Scripts\\activate && pip install django && pip install django-allauth && cd Project1 && python manage.py runserver")
