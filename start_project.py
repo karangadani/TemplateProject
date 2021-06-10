e76aeafd2e65b1c0d47a665b340402de862e1ac4
Karan Gadani <52932210+karangadani@users.noreply.github.com>
2021-06-10 11:55:55 +0530

    Update start_project.py

--- start_project.py
+++ start_project.py
@@ -3,4 +3,4 @@ import os
 WORKDIR = os.getcwd()
 
 os.chdir(WORKDIR)
-os.system(f"cd .. && pip install virtualenv && virtualenv venv && venv\\Scripts\\activate && pip install django && pip install django-allauth && cd Project1 && python manage.py runserver")
+os.system(f"pip install virtualenv && virtualenv venv && venv\\Scripts\\activate && pip install django && pip install django-allauth && cd Project1 && python manage.py runserver")
